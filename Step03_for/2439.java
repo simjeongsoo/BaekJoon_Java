9b67b62ecedba7d2604bd484107954d1e07e6d4e
simjeongsoo <simking95@naver.com>
2022-03-02 22:12:05 +0900

    Step03-10

---
+++ Step03_for/2439.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+ 
+public class Main {
+ 
+	public static void main(String[] args) {
+		Scanner scr = new Scanner(System.in);
+ 
+		int N = scr.nextInt();
+		scr.close();
+ 
+		for (int i = 1; i <= N; i++) {
+			for (int j = 1; j <= N - i; j++) {
+				System.out.print(" ");
+			}
+			for (int k = 0; k < i; k++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
