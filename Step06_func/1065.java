2794fc48eb055f5197c370ba63879b257b19ef42
simjeongsoo <simking95@naver.com>
2022-03-19 16:13:58 +0900

    Step06-03

---
+++ Step06_func/1065.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+ 
+public class Main {
+ 
+	public static void main(String[] args) {
+		Scanner scr = new Scanner(System.in);
+		
+		System.out.print(arithmetic_sequence(scr.nextInt()));
+		scr.close();
+ 
+		
+	}
+ 
+	public static int arithmetic_sequence(int num) {
+		int cnt = 0;
+ 
+		if (num < 100) {
+			return num;
+		}
+ 
+		else {
+			cnt = 99;
+			if (num == 1000) {
+				num = 999;
+			}
+ 
+			for (int i = 100; i <= num; i++) {
+				int hun = i / 100;
+				int ten = (i / 10) % 10; 
+				int one = i % 10;
+ 
+				if ((hun - ten) == (ten - one)) {
+					cnt++;
+				}
+			}
+		}
+
+		return cnt;
+	} 
+}
