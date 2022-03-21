34eef67dc91b28bc1a486a893b8def569bb5bafb
simjeongsoo <simking95@naver.com>
2022-03-21 17:32:09 +0900

    Step07-01

---
+++ Step07_string/11654.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+ 
+public class Main {
+	public static void main(String[] args) {
+ 
+		Scanner scr = new Scanner(System.in);
+ 
+		int ch = scr.next().charAt(0);
+        
+		System.out.print(ch);
+	}
+}
