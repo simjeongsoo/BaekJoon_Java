182fd53e7f330b7fda209da8de6b2804886ed815
simjeongsoo <simking95@naver.com>
2022-06-13 23:47:45 +0900

    Step08-08

---
+++ Step08_math01/10757.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+ 
+public class Main {
+	public static void main(String[] args) {
+		
+		Scanner scr = new Scanner(System.in);
+		
+		String str_A = scr.next();
+		String str_B = scr.next();
+		
+		int max_length = Math.max(str_A.length(), str_B.length());
+		
+		int[] A = new int[max_length + 1];	
+		int[] B = new int[max_length + 1];	
+		
+		for(int i = str_A.length() - 1, idx = 0; i >= 0; i--, idx++) {
+			A[idx] = str_A.charAt(i) - '0';	
+		}
+ 
+		for(int i = str_B.length() - 1, idx = 0; i >= 0; i--, idx++) {
+			B[idx] = str_B.charAt(i) - '0';	
+		}
+		
+		for(int i = 0; i < max_length; i++) {
+			int value = A[i] + B[i];
+			A[i] = value % 10;		
+			A[i + 1] += (value / 10);
+		}
+		
+		StringBuilder sb = new StringBuilder();
+		if(A[max_length] != 0) {
+			sb.append(A[max_length]);
+		}
+		for(int i = max_length - 1; i >= 0; i--) {
+			sb.append(A[i]);
+		}
+		
+		System.out.println(sb);
+	}
+}
